4fc8f4083991ea88d52c8e1996e3ecb7ae126090
enzo.gelas <enzo.gelas@telecom-paris.fr>
2023-06-26 15:55:45 +0200

    ajout beat tracking

--- beat_tracking/spectro.py
+++ beat_tracking/spectro.py
@@ -27,9 +27,7 @@ i=complex(0,1)
 pi=np.pi 
 
 
-# lecture de donn�es sauvegard�es au format matlab LECTURE AEIOU.MAT
-
-audio_data,sample_rate = librosa.load('mrbluesky.mp3')
+audio_data,sample_rate = librosa.load('happy.mp3')
 clap_data,clap_rate = librosa.load('clap.mp3')
 long_clap = len(clap_data)
 #audio_data = real(np.asarray((4000*[0]+[exp(2*i*pi*0.02*k) for k in range(4000)])*30))
@@ -92,6 +90,8 @@ def lognozero(x):
 plt.imshow(lognozero(spec))
 show()
 
+# créer le spectrogramme des variations
+
 (i,j)=np.shape(spec)
 
 specdiff=np.zeros((i,j))
@@ -102,6 +102,8 @@ for k in range(1,j):
 
 plt.imshow(specdiff)
 show()
+
+#Faire la moyenne sur cha<ue colonne du spectrogramme
 
 def moy(tab):
     S=0
@@ -115,9 +117,28 @@ Y=[]
 for k in range(j):
     Y.append(moy(specdiff[:, k]))
 Y=np.asarray(Y)
+
+#enlever la moyenne locale à chaque point
+
+def withoutmoy(curb,largmoy):
+    newcurb=curb
+    for k in range(0,len(curb)):
+        Smoy = 0
+        taillecomp = 0
+        for xcomp in range(max(0,k-largmoy),min(k+largmoy+1,len(curb))):
+            Smoy = Smoy + curb[xcomp]
+            taillecomp = taillecomp + 1
+        Smoy = Smoy/taillecomp
+        newcurb[k] = max(0,newcurb[k] - Smoy)      
+    return(newcurb)
+
+Y = withoutmoy(Y,10) 
+
 plot(X,Y)
 plt.xlabel('temps (s)')
 show()
+
+#trouver les maximums d'une courbe
 
 def maxima(curb,larg):
     Max=[]
@@ -131,7 +152,6 @@ def maxima(curb,larg):
     print(len(Max))
     return(Max)
 
-#Calculer l'écart entre les beats = tempo
 
 S=0
 tps_verif = int(0.2*Fe*len(Y)/len(audio_data)) # 0.2 seconde de tolérance pour détecter les maxima
@@ -158,9 +178,9 @@ def tempo_from_correl(cor):
     M = maxima(cor,tps_verif)
     print(M)
     S = 0
-    for k in range(6): #on moyenne les 4 premiers recouvrements dans l'autocorrélation
-        S = S + M[k]/(k+1)
-    S = S/6
+    for k in range(4): #on moyenne les 4 premiers recouvrements dans l'autocorrélation
+        S = S + M[k+1]-M[k]
+    S = S/4
     return(S*len(audio_data)*Te/len(cor))  # Avant : M[1]*len(audio_data)*Te/len(cor) ou S*len(audio_data)*Te/len(cor) 
 
 #Ajout des beats
@@ -178,7 +198,7 @@ print(tempo)
 pas_ind_tempo = int(tempo*Fe)
 """
 i = 0 # indice pour parcourir le tableau TabMax
-k = TabMax[0]
+k = TabMax[1]
 ind = int(k*len(audio_data)/len(Y))
 while ind<len(audio_data):
     if ind+long_clap > len(audio_data): #pour éviter d'arriver au bord
@@ -198,7 +218,7 @@ while ind<len(audio_data):
         if ind+long_clap > len(audio_data): #pour éviter d'arriver au bord
             break
         else:
-            audio_data[ind:ind+long_clap] = audio_data[ind:ind+long_clap]+10*clap_data # on a un indice particulier pour le clap si jamais il est ajouté en fin de musique
+            audio_data[ind:ind+long_clap] = audio_data[ind:ind+long_clap]+20*clap_data # on a un indice particulier pour le clap si jamais il est ajouté en fin de musique
             ind = ind + pas_ind_tempo
     k = int(ind*len(Y)/len(audio_data))
     
@@ -223,8 +243,20 @@ audio_data_int = (normalized_data * 32767).astype(np.int16)
 scipy.io.wavfile.write('output.wav', sample_rate, audio_data_int)
 
 """
-hop_length = 512
-tempogram = librosa.feature.tempogram(y=audio_data, sr=sample_rate, hop_length=hop_length)
-plt.imshow(tempogram)
+Fe = 20
+N = 200
+M = 200
+nb = 10
+spectps = affiche_spectrogramme(Y,N,M=M,nb=nb,Fe=Fe)
+plt.imshow(spectps)
 show()
+
+(itps,jtps) = np.shape(spectps)
+print(itps,jtps)
+for l in range(jtps):
+    argmax = 0
+    for k in range(itps):
+        if spectps[k,l]>spectps[argmax,l]:
+            argmax = k 
+    print(1/(argmax*(Fe/2)/itps))
 """
